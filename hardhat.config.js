a2a55925b74dbdf8fd382e3fe1812910699bc341
Ishrani Rajiv <rajivishrani@gmail.com>
2022-10-28 13:33:28 +0530

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,36 +1,36 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("solidity-coverage");
-require("hardhat-deploy");
-require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox")
+require("solidity-coverage")
+require("hardhat-deploy")
+require("dotenv").config()
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
-const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 module.exports = {
-  solidity: "0.8.17",
-  networks: {
-    goerli: {
-      url: GOERLI_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 5,
-      blockConfirmations: 6,
-    },
+    solidity: "0.8.17",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
 
-    mumbai: {
-      url: MUMBAI_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 80001,
-      blockConfirmations: 6,
+        mumbai: {
+            url: MUMBAI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 80001,
+            blockConfirmations: 6,
+        },
     },
-  },
-  etherscan: {
-    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-    apiKey: {
-      goerli: ETHERSCAN_API_KEY,
-      polygon: POLYGONSCAN_API_KEY,
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+            polygonMumbai: POLYGONSCAN_API_KEY,
+        },
     },
-  },
-};
+}
