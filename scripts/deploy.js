6af5375c0f949532947b9c4f4e16bd4e111e51d6
Ishrani Rajiv <rajivishrani@gmail.com>
2022-10-28 13:33:58 +0530

    delpoyment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,13 @@
 const hre = require("hardhat")
 
-async function main() {}
+async function main() {
+    const Factory = await hre.ethers.getContractFactory("Factory")
+    const factoryInstance = await Factory.deploy()
+    await factoryInstance.deployed()
+    console.log(factoryInstance)
+    console.log("--------------------Deployed--------------------")
+    console.log("Factory Address: ", factoryInstance.address)
+}
 
 main().catch((error) => {
     console.error(error)
