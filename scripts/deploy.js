e961e2cdd681d0207edfe1ca7b27fe3acf5057d8
Ishrani Rajiv <rajivishrani@gmail.com>
2022-11-03 18:28:58 +0530

    updated: deployment script for ERC721Factory contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,12 @@
 const hre = require("hardhat")
 
 async function main() {
-    const Factory = await hre.ethers.getContractFactory("Factory")
-    const factoryInstance = await Factory.deploy()
-    await factoryInstance.deployed()
-    console.log(factoryInstance)
+    const ERC721Factory = await hre.ethers.getContractFactory("ERC721Factory")
+    const erc21FactoryInstance = await ERC721Factory.deploy()
+    await erc21FactoryInstance.deployed()
+    console.log(erc21FactoryInstance)
     console.log("--------------------Deployed--------------------")
-    console.log("Factory Address: ", factoryInstance.address)
+    console.log("Factory Address: ", erc21FactoryInstance.address)
 }
 
 main().catch((error) => {
