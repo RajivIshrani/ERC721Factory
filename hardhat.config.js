c9e1849f22bd36526758fe218bc46e46b3b18b59
Ishrani Rajiv <rajivishrani@gmail.com>
2022-11-08 14:43:44 +0530

    updated: localhost network

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,9 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 module.exports = {
     solidity: "0.8.17",
     networks: {
+        localhost: {
+            url: "http://127.0.0.1:8545",
+        },
         goerli: {
             url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY],
