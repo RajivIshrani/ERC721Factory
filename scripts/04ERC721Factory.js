f5ebda98bbd04892285e8632ef1f9c420d5c87f9
Ishrani Rajiv <rajivishrani@gmail.com>
2022-11-18 12:34:40 +0530

    calling deployNFTContract using sendTransaction

--- scripts/04ERC721Factory.js
+++ scripts/04ERC721Factory.js
@@ -2,7 +2,7 @@ const { ethers } = require("hardhat")
 const abi = require("../artifacts/contracts/MintAndStorage/ERC721Factory.sol/ERC721Factory.json")
 
 const provider = new ethers.providers.JsonRpcProvider(
-    "http://localhost:8545/%22"
+    "http://localhost:8545"
 )
 console.log(provider)
 
@@ -10,6 +10,9 @@ const abicoder = new ethers.utils.AbiCoder()
 
 const main = async () => {
     const [account1, account2] = await ethers.getSigners()
+    const bal = await account1.getBalance()
+    
+    console.log(account1.getBalance)
     // Creating Instance of ERC721Factory
 
     console.log("\n---------- Creating Instance of ERC721Factory ----------\n")
@@ -20,20 +23,25 @@ const main = async () => {
     contractAddress = erc721FactoryInstance.address
     console.log(`\nERC721Factory Address --> ${contractAddress}\n`)
 
-    const dataEncode = abicoder.encode(["string", "string"], ["TOKEN", "TKN"])
-    console.log(`\ndata --> ${dataEncode}\n`)
+    let interface = new ethers.utils.Interface(abi.abi)
+
+    const dataEncode = interface.encodeFunctionData("deployNFTContract", ["TOKEN", 'TKN'])
 
     const nonce = await provider.getTransactionCount(account1.address)
-
+    console.log(nonce)
     const tx = {
         nonce: nonce,
         from: account1.address,
         to: erc721FactoryInstance.address,
         data: dataEncode,
-        gasLimit: "25000",
+        // gasLimit: "25000",
+        // gasPrice: "10000000"
     }
+    console.log(tx)
+
 
     const sendTx = await account1.sendTransaction(tx)
+    console.log(sendTx)
     const sendTxReceipt = await sendTx.wait()
     console.log(sendTxReceipt)
 }
