77160873f2a37f597168b66621d015365f4d6e06
Ishrani Rajiv <rajivishrani@gmail.com>
2022-11-26 00:19:21 +0530

    add from address, and txHash

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,8 +5,14 @@ async function main() {
     const erc21FactoryInstance = await ERC721Factory.deploy()
     await erc21FactoryInstance.deployed()
     console.log(erc21FactoryInstance)
-    console.log("--------------------Deployed--------------------")
-    console.log("Factory Address: ", erc21FactoryInstance.address)
+    console.log("\n--------------------Deployed--------------------\n")
+    const txHash = await erc21FactoryInstance.deployTransaction.hash
+    console.log(`TxHash --> ${txHash}\n`)
+
+    const from = await erc21FactoryInstance.deployTransaction.from
+    console.log(`From --> ${from}\n`)
+
+    console.log(`Factory Address --> ${erc21FactoryInstance.address}`)
 }
 
 main().catch((error) => {
